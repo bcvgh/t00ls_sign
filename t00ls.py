b8c253d64dfc5d15180d98b01e53a216841c7bf2
bcvgh <56790427+bcvgh@users.noreply.github.com>
2022-09-16 10:01:58 +0800

    Update t00ls.py

--- t00ls.py
+++ t00ls.py
@@ -1,12 +1,13 @@
+#encoding:utf-8
 import json
 import requests
 import bs4
 import re
 requests.packages.urllib3.disable_warnings()
-username = '***********'  # 帐号
-password = '******************'  # 密码MD5 32位(小写)
-question_num = '*'  # 安全提问 参考下面
-question_answer = '**'  # 安全提问答案
+username = '***'  # 帐号
+password = '***'  # 密码MD5 32位(小写)
+question_num = *  # 安全提问 参考下面
+question_answer = '**'  # 安全提问答案,可以是汉字
 proxies = {
     'https': "http://127.0.0.1:8080",
     'http': "http://127.0.0.1:8080"
@@ -29,7 +30,7 @@ def t00ls_login(u_name, u_pass, q_num, q_ans):
         'loginsubmit':'登录',
         'redirect':'https://www.t00ls.com'
     }
-    response_login = requests.post('https://www.t00ls.com/login.html', proxies=proxies, verify=False, headers=headers)
+    response_login = requests.post('https://www.t00ls.com/login.html', verify=False, headers=headers)
     soup = bs4.BeautifulSoup(response_login.text, 'lxml')
     # if response_login_json['status'] != 'success':
     #     return None
@@ -38,7 +39,7 @@ def t00ls_login(u_name, u_pass, q_num, q_ans):
         formhash1 = {"formhash": formhash}
         login_data.update(formhash1)
         response_login1 = requests.post(url='https://www.t00ls.com/login.html', data=login_data, headers=headers,
-                                        verify=False, proxies=proxies,)
+                                        verify=False,)
         if response_login1.history[0].status_code == 302:
             cookie_value = requests.utils.dict_from_cookiejar(response_login1.history[0].cookies)
 
@@ -65,19 +66,20 @@ def t00ls_sign(t00ls_hash, t00ls_cookies):
     # t00ls_cookies = {"Cookie": t00ls_cookies}
     # headers.update(t00ls_cookies)
     response_sign = requests.post('https://www.t00ls.com/ajax-sign.json', cookies=t00ls_cookies, data=sign_data,
-                                  verify=False, proxies=proxies, headers=headers)
+                                  verify=False, headers=headers)
     soup = bs4.BeautifulSoup(response_sign.text, 'lxml')
     s=soup.find_all(href=re.compile("formhash"))
     pattern=re.compile('[0-9a-zA-Z]{8}')
     formhash = {"formhash": pattern.findall(str(s[0]))[1]}
     sign_data.update(formhash)
-    response_sign = requests.post('https://www.t00ls.com/ajax-sign.json', cookies=t00ls_cookies, data=sign_data, verify=False, proxies=proxies, headers=headers)
+    response_sign = requests.post('https://www.t00ls.com/ajax-sign.json', cookies=t00ls_cookies, data=sign_data, verify=False, headers=headers)
     return json.loads(response_sign.text)
 
 
 def main():
     response_login = t00ls_login(username, password, question_num, question_answer)
     if response_login:
+	print('登录成功')
         response_sign = t00ls_sign(response_login[0], response_login[1])
         if response_sign['status'] == 'success':
             print('签到成功')
